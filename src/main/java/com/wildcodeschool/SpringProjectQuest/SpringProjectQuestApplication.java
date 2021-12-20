6e268ab91da8372d762be01896f1c941eae8bf81
arsiasieg <cam.lucas89@gmail.com>
2021-12-20 11:46:33 +0100

    routing

---
+++ src/main/java/com/wildcodeschool/SpringProjectQuest/SpringProjectQuestApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.SpringProjectQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/SpringProjectQuest/SpringProjectQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.SpringProjectQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
